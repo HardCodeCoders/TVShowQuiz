1ec0c2190a79caa606418708f07f29c59fd937b9
Piotr <urban199191@gmail.com>
2021-01-17 14:51:17 +0100

    build: code refactor,add scss preprocessor

--- webpack.config.js
+++ webpack.config.js
@@ -40,3 +40,81 @@ module.exports = {
     ],
   },
 };
+
+// const path = require("path");
+
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+// module.exports = {
+//   mode: "development",
+//   entry: "./src/main.js",
+//   output: {
+//     path: path.resolve(__dirname, 'build'),
+//     filename: "js/main.js",
+//     publicPath: '/assets/'
+//   },
+//   devServer: {
+//     port: 9000,
+//     contentBase: path.join(__dirname, 'dist')
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader"
+//         }
+//       },
+//       {
+//         test: /\.html$/,
+//         use: [
+//           {
+//             loader: "html-loader",
+//             options: { minimize: true }
+//           }
+//         ]
+//       },
+//       {
+//         test: /\.(png|svg|jpe?g|gif)$/i,
+//         use: [
+//           {
+//             loader: "file-loader",
+//           },
+//         ],
+//       },
+//       {
+//         test: /\.s[ac]ss$/i,
+//         use: [
+//           // Creates `style` nodes from JS strings
+//           "style-loader",
+//           // Translates CSS into CommonJS
+//           "css-loader",
+//           // Compiles Sass to CSS
+//           "sass-loader",
+//         ],
+//       },
+//       {
+//         test: /\.(css)$/i,
+//         use: [
+//           // Creates `style` nodes from JS strings
+//           "style-loader",
+//           // Translates CSS into CommonJS
+//           "css-loader",
+//         ],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: "./dist/index.html",
+//       filename: "./index.html"
+//     }),
+//     new MiniCssExtractPlugin({
+//       filename: "[name].css",
+//       chunkFilename: "[id].css"
+//     })
+//   ]
+// };
