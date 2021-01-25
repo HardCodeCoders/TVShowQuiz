b66bcdb7f1ddb1cc1f836f653844f8994d6b0ff3
anpastor <ania.pastor@gmail.com>
2021-01-25 10:37:36 +0100

    refactor

--- src/updateRanking.js
+++ src/updateRanking.js
@@ -17,41 +17,41 @@ button.addEventListener('click', saveToLocalStorage)
 
 
 
-// export function updateRanking({ name, points, difficultyLevel: category }) {
-//     let rankingStorage = localStorage.getItem(category);
-//     if (!rankingStorage) {
-//         let rankingObject = {
-//             category: category,
-//             scores: [
-//                 {
-//                     name: name,
-//                     score: points,
-//                 },
-//             ],
-//         };
-//         localStorage.setItem(category, JSON.stringify(rankingObject));
-//         return rankingObject;
-//     }
+export function updateRanking({ name, points, difficultyLevel: category }) {
+    let rankingStorage = localStorage.getItem(quizName);
+    if (!rankingStorage) {
+        let rankingObject = {
+            category: quizName,
+            scores: [
+                {
+                    name: name,
+                    score: points,
+                },
+            ],
+        };
+        localStorage.setItem(category, JSON.stringify(rankingObject));
+        return rankingObject;
+    }
 
-//     let rankingData = JSON.parse(rankingStorage);
-//     let isPlayerInRanking = rankingData.scores.some((player) => {
-//         return player.name == name;
-//     });
-//     if (isPlayerInRanking) {
-//         let currentPlayerIndex = rankingData.scores.findIndex((player) => {
-//             return player.name == name;
-//         });
-//         if (rankingData.scores[currentPlayerIndex].score <= points) {
-//             rankingData.scores[currentPlayerIndex].score = points;
-//         }
-//     } else {
-//         rankingData.scores.push({ name: name, score: points });
-//     }
+    let rankingData = JSON.parse(rankingStorage);
+    let isPlayerInRanking = rankingData.scores.some((player) => {
+        return player.name == name;
+    });
+    if (isPlayerInRanking) {
+        let currentPlayerIndex = rankingData.scores.findIndex((player) => {
+            return player.name == name;
+        });
+        if (rankingData.scores[currentPlayerIndex].score <= points) {
+            rankingData.scores[currentPlayerIndex].score = points;
+        }
+    } else {
+        rankingData.scores.push({ name: name, score: points });
+    }
 
-//     rankingData.scores.sort((player1, player2) => {
-//         return player2.score - player1.score;
-//     });
+    rankingData.scores.sort((player1, player2) => {
+        return player2.score - player1.score;
+    });
 
-//     localStorage.setItem(category, JSON.stringify(rankingData));
-//     return rankingData;
-// }
+    localStorage.setItem(category, JSON.stringify(rankingData));
+    return rankingData;
+}
